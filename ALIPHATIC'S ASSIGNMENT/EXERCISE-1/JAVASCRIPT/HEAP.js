66d02632d2fb7c2954e805865955fcda42ae5e0d
ALIPHATIC <adegokefuad1@gmail.com>
2023-07-11 01:06:03 +0100

    Question 1

---
+++ ALIPHATIC'S ASSIGNMENT/EXERCISE-1/JAVASCRIPT/HEAP.js
@@ -0,0 +1,12 @@
+/* EXERCISE 1 ON HEAP
+
+Implement a heap data structure storing numeric data in your chosen language with the following operations
+heapify() - balance a heap 
+insert(item) - add a new item to a heap
+find(item) - returns the node containing search item.
+findParent(item) - returns parent of an item
+findChildren(item) - returns children of an item
+remove(item) - remove an item
+traverse() - print out values from top to down
+heapsort() - sort the items of a heap
+*/
